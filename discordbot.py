0e540d59e55d7768788910ee3c7d04a2c00ab993
f2019116 <82922807+f2019116@users.noreply.github.com>
2021-05-31 11:42:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-    async def 'やっほー'(ctx):
+    async def やっほー(ctx):
     await ctx.send('やっほー')
 
 
