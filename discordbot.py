0b26e34a0776f2f51eaab95124b98a725375739b
f2019116 <82922807+f2019116@users.noreply.github.com>
2021-05-31 11:48:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,7 +19,7 @@ async def ping(ctx):
     
 @client.event
 async def on_message(message):
-   if message.author.bot:　　　　　　　　　　　　　　　
+   if message.author.bot:
        return  　　　　　　　　＃発言者がBOTなら無視
    if message.content == 'やっほー':　　　　　＃発言に返信する
        await message.channel.send('やっほー')　＃返信内容は''で括る。
